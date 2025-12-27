9009ee152ccfdf1f9240f590fdad5b2639a2efbd
Benoit Chan Fook <benoit.chan-fook@sorbonne-nouvelle.fr>
2025-12-28 00:23:43 +0100

    Ajout PALS anglais

---
+++ programmes/programmes_anglais/make_pals_corpus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+DOSSIER="$1"
+
+NOM_BASE="$2"
+
+> "pals/${NOM_BASE}.txt"
+
+for fichier in "$DOSSIER"/*.txt
+do
+
+    tr -cs "[:alpha:]" "\n" < "$fichier" >> "pals/${NOM_BASE}.txt"
+done
+
+echo "Traitement terminé. Le fichier pals/${NOM_BASE}.txt est prêt."
